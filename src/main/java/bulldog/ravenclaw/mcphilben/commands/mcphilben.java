f5d71d215405c080e5f4a313c67289a3e02532c1
Gradient <graident.p@gmail.com>
2023-04-15 22:56:50 -0400

    Added a command. i need to learn this

--- src/main/java/bulldog/ravenclaw/mcphilben/commands/mcphilben.java
+++ src/main/java/bulldog/ravenclaw/mcphilben/commands/mcphilben.java
@@ -1,4 +1,21 @@
 package bulldog.ravenclaw.mcphilben.commands;
 
-public class mcphilben {
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import meteordevelopment.meteorclient.systems.commands.Command;
+import net.minecraft.command.CommandSource;
+
+import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+public class mcphilben extends Command {
+    public mcphilben() {
+        super("McPhilben", "Base command for the McPhilben addon");
+    }
+
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> builder) {
+        builder.executes(context -> {
+            info("McPhilben is loaded :)");
+            info("run the x-test command for debug");
+            return SINGLE_SUCCESS;
+        });
+    }
 }
