f68914e72f38850686541047a907cf91644f893b
Gradient <graident.p@gmail.com>
2023-04-19 20:29:35 -0400

    Updating so i can push

--- src/main/java/bulldog/ravenclaw/mcphilben/modules/ModuleExample.java
+++ src/main/java/bulldog/ravenclaw/mcphilben/modules/ModuleExample.java
@@ -5,6 +5,6 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 
 public class ModuleExample extends Module {
     public ModuleExample() {
-        super(McPhilben.CATEGORY, "example", "An example module in a custom category.");
+        super(McPhilbenClient.CATEGORY, "example", "An example module in a custom category.");
     }
 }
